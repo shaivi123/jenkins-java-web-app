001664b919e4a1be570fbb8b54a5ee9252e742c1
VIJAY\jignesh <jignesh@appstle.com>
2024-03-21 11:41:16 +0530

    first commit

---
+++ src/main/java/com/jenkins/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(Model model){
+        model.addAttribute("message","Hello World!");
+        return "welcome";
+    }
+}
